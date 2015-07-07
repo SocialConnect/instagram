c45e8735efbd1cdd467f2b7c82eff130af9b10f3
Dmitry Patsura <talk@dmtry.me>
2015-07-07 13:21:36 +0900

    Write Client

--- src/Client.php
+++ src/Client.php
@@ -19,4 +19,58 @@ class Client extends \SocialConnect\Common\ClientAbstract
     {
         $this->accessToken = $accessToken;
     }
+
+    protected function checkLimit($limit)
+    {
+        if (!is_int($limit) || $limit <= 0) {
+            throw new \InvalidArgumentException('$limit must be > 0, actual: ' . $limit);
+        }
+    }
+
+    public function searchUser($name, $limit = null)
+    {
+        if (!is_null($limit)) {
+            $this->checkLimit($limit);
+        }
+    }
+
+    public function getUser($id = 0)
+    {
+
+    }
+
+    public function getUserFeed($limit = null)
+    {
+        if (!is_null($limit)) {
+            $this->checkLimit($limit);
+        }
+    }
+
+    public function getUserMedia($id = 'self', $limit = null)
+    {
+        if (!is_null($limit)) {
+            $this->checkLimit($limit);
+        }
+    }
+
+    public function getUserLikes($limit = 0)
+    {
+        if (!is_null($limit)) {
+            $this->checkLimit($limit);
+        }
+    }
+
+    public function getUserFollows($id = 'self', $limit = null)
+    {
+        if (!is_null($limit)) {
+            $this->checkLimit($limit);
+        }
+    }
+
+    public function getUserFollower($id = 'self', $limit = null)
+    {
+        if (!is_null($limit)) {
+            $this->checkLimit($limit);
+        }
+    }
 }
