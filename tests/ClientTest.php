56ec8122efef8c21ba34579225027bc7f3ac2158
Dmitry Patsura <talk@dmtry.me>
2015-07-09 17:26:39 +0900

    Working on tests...

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -5,8 +5,39 @@
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @return \SocialConnect\Instagram\Client
+     */
+    protected function getClient()
+    {
+        $client = new \SocialConnect\Instagram\Client($GLOBALS['applicationId'], $GLOBALS['applicationSecret']);
+        return $client;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getAccessToken()
+    {
+        return $GLOBALS['testUserAccessToken'];
+    }
+
+
+    /**
+     * @return mixed
+     */
+    protected function getDemoUserId()
+    {
+        return $GLOBALS['testUserId'];
+    }
+
     public function testGetUser()
     {
-        $this->assertTrue(true);
+        $client = $this->getClient();
+        $client->setAccessToken($this->getAccessToken());
+
+        $user = $client->getUser($this->getDemoUserId());
+        var_dump($user);
+        die();
     }
 }
