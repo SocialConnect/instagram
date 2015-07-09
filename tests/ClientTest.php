7ec6e4823c97cd0564072727603fdba7a34982fc
Dmitry Patsura <talk@dmtry.me>
2015-07-09 17:14:28 +0900

    Added base tests skeleton

---
+++ tests/ClientTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Patsura Dmitry <talk@dmtry.me>
+ */
+
+class ClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetUser()
+    {
+        $this->assertTrue(true);
+    }
+}
