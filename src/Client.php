4dabac96073981883c0fdd02f0635f46f87f6764
Dmitry Patsura <talk@dmtry.me>
2015-07-09 18:31:01 +0900

    Client - implement request method

--- src/Client.php
+++ src/Client.php
@@ -14,11 +14,54 @@ class Client extends \SocialConnect\Common\ClientAbstract
     use HttpClient;
 
     /**
+     * @var string
+     */
+    protected $apiUrl = 'https://api.instagram.com/v1/';
+
+    /**
      * @param string $accessToken
      */
     public function setAccessToken($accessToken)
     {
         $this->accessToken = $accessToken;
+    }
+
+    /**
+     * @param $uri
+     * @param array $parameters
+     * @param bool|false $accessToken
+     * @return bool|mixed
+     * @throws \Exception
+     */
+    public function request($uri, array $parameters = array(), $accessToken = false)
+    {
+        $parameters['client_id'] = $this->appId;
+        $parameters['access_token'] = $this->accessToken;
+
+        foreach ($parameters as $key => $parameter) {
+            if (is_array($parameter)) {
+                $parameters[$key] = implode(',', $parameter);
+            }
+        }
+
+        $response = $this->httpClient->request($this->apiUrl . $uri . '?' . http_build_query($parameters), []);
+        if ($response) {
+            if ($response->isServerError()) {
+                throw new \Exception($response);
+            }
+            $body = $response->getBody();
+            if ($body) {
+                $json = json_decode($body);
+                if ($json->data) {
+                    return $json->data;
+                }
+                
+                throw new \Exception($response);
+            } else {
+                throw new \Exception($response);
+            }
+        }
+        return false;
     }
 
     protected function checkLimit($limit)
