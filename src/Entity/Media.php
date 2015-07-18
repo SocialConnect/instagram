503349386e703b2b69a7898bee86d39a4c365416
Dmitry Patsura <talk@dmtry.me>
2015-07-18 23:07:40 +0900

    Entity\Media - added properties for autocomplete

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -8,5 +8,65 @@ namespace SocialConnect\Instagram\Entity;
 
 class Media extends \stdClass
 {
+    /**
+     * @var string
+     */
+    public $id;
 
+    /**
+     * @var array|null
+     */
+    public $attribution;
+
+    /**
+     * @var array
+     */
+    public $tags;
+
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var string
+     */
+    public $location;
+
+    /**
+     * @var array
+     */
+    public $comments;
+
+    public $caption;
+
+    /**
+     * What's filter was used
+     * @var string
+     */
+    public $filter;
+
+    /**
+     * Unixtime
+     * @var string
+     */
+    public $created_time;
+
+    /**
+     * @var string
+     */
+    public $link;
+
+    /**
+     * @var \stdClass
+     */
+    public $likes;
+
+    public $image;
+
+    public $users_in_photo;
+
+    public $user_has_liked;
+
+    public $user;
 }
